f98dc083a56ce0647cf41a3fb9e2a74f3f6a8af4
Kevin Palms <kevin@stamped.com>
2011-07-05 22:59:45 -0400

    iOS Fonts Test
    Quick demo to illustrate a problem that Xcode / Cocoa is having recognizing the Light version of the Titling Gothic.

---
+++ internal/projects/ios-fonts-test/FontTest/FontTest/FontTestAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  FontTestAppDelegate.h
+//  FontTest
+//
+//  Created by Kevin Palms on 7/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FontTestAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ internal/projects/ios-fonts-test/FontTest/FontTest/FontsViewController.h
@@ -0,0 +1,16 @@
+//
+//  FontsViewController.h
+//  FontTest
+//
+//  Created by Kevin Palms on 7/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FontsViewController : UIViewController {
+    
+}
+
+@end
