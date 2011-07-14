671431472d0ea6e6f588d96c426065d445921716
Kevin Palms <kevin@stamped.com>
2011-07-13 22:30:31 -0400

    GitHub for Mac appears to randomly roll back some of my files
    WTF GITHUB

--- internal/projects/api/db/mongodb/MongoFriendship.py
+++ internal/projects/api/db/mongodb/MongoFriendship.py
@@ -14,70 +14,82 @@ from MongoDB import Mongo
 from AFriendshipDB import AFriendshipDB
 from Friendship import Friendship
 from MongoUser import MongoUser
+from MongoFriends import MongoFriends
+from MongoFollowers import MongoFollowers
 
-class MongoFriendship(AFriendshipDB, Mongo):
-        
-    COLLECTION = 'friendships'
+class MongoFriendship(AFriendshipDB):
         
     SCHEMA = {
         '_id': basestring,
-        'friend_id': basestring,
-        'timestamp': basestring
+        'friend_id': basestring
     }
+    
+    DESC = 'Mongo Friends Wrapper'
     
     def __init__(self, setup=False):
         AFriendshipDB.__init__(self, self.DESC)
-        Mongo.__init__(self, collection=self.COLLECTION)
-        
-        self.db = self._getDatabase()
-        self._lock = Lock()
         
         
     ### PUBLIC
     
     def addFriendship(self, friendship):
-        
+    
         friendship = self._objToMongo(friendship)
+        userId = friendship['_id']
+        friendId = friendship['friend_id']
         
         # Check if friendship already exists (???)
         
         # Check if block exists
         
         # Check if following_id is private
-        if MongoUser().checkPrivacy(friendship['friend_id']):
+        if MongoUser().checkPrivacy(userId):
             # Request approval before creating friendship
             print 'TODO: Locked account friendship request'
             return False
             
         else:
             # Create friendship
-            self._createRelationship(keyId=friendship['_id'], refId=friendship['friend_id'])
-#             self._createFollower(userId=friendship['friend_id'], followerId=friendship['_id'])
+            MongoFriends().addFriend(userId=userId, friendId=friendId)
+            MongoFollowers().addFollower(userId=friendId, followerId=userId)
             return True
-    
+            
     def checkFriendship(self, friendship):
-        return self._checkRelationship(keyId=friendship['user_id'], 
-                                        refId=friendship['friend_id'])
+        friendship = self._objToMongo(friendship)
+        return MongoFriends().checkFriend(userId=friendship['_id'], friendId=friendship['friend_id'])
             
     def removeFriendship(self, friendship):
-        return self._removeRelationship(keyId=friendship['user_id'], 
-                                        refId=friendship['friend_id'])
+    
+        friendship = self._objToMongo(friendship)
+        userId = friendship['_id']
+        friendId = friendship['friend_id']
+        
+        try:
+            MongoFriends().removeFriend(userId=userId, friendId=friendId)
+            MongoFollowers().removeFollower(userId=friendId, followerId=userId)
+            return True
+        except:
+            return False
             
-    def getFriendships(self, userId):
-        return self._getRelationships(userId)
+    def getFriends(self, userId):
+        return MongoFriends().getFriends(userId)
+            
+    def getFollowers(self, userId):
+        return MongoFollowers().getFollowers(userId)
 
     def approveFriendship(self, friendship):
         ### TODO
         print 'TODO'
-
-
+            
+            
     ### PRIVATE
-    
+
     def _objToMongo(self, obj):
         if obj.isValid == False:
             print obj
             raise KeyError('Object not valid')
-        data = copy.copy(obj.getDataAsDict())
-        data['_id'] = data['user_id']
-        del(data['user_id'])
-        return self._mapDataToSchema(data, self.SCHEMA)
+        data = {}
+        data['_id'] = obj.user_id
+        data['friend_id'] = obj.friend_id
+        return data
+
