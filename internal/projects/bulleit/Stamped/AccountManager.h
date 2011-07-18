083905b97cfa8a99e36324717355ccf8dc419dc1
Andrew Bonventre <andybons@gmail.com>
2011-07-18 19:42:58 -0400

    Borders for stamp detail view. Skeleton account management/login.

---
+++ internal/projects/bulleit/Stamped/AccountManager.h
@@ -0,0 +1,13 @@
+//
+//  AccountManager.h
+//  Stamped
+//
+//  Created by Andrew Bonventre on 7/18/11.
+//  Copyright 2011 Stamped, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AccountManager : NSObject
+
+@end
