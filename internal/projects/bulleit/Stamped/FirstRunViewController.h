9986e69394ed5cfff4d418a0a93740203ca6812a
Andrew Bonventre <andybons@gmail.com>
2011-09-05 15:18:43 -0400

    wow. github for mac you SUCK.

--- internal/projects/bulleit/Stamped/FirstRunViewController.h
+++ internal/projects/bulleit/Stamped/FirstRunViewController.h
@@ -1,27 +1,18 @@
 //
-//  LearnMoreViewController.h
+//  FirstRunViewController.h
 //  Stamped
 //
 //  Created by Jake Zien on 9/4/11.
-//  Copyright 2011 RISD. All rights reserved.
+//  Copyright Stamped, Inc. All rights reserved.
 //
 
 #import <QuartzCore/QuartzCore.h>
 #import <UIKit/UIKit.h>
 
-@interface LearnMoreViewController : UIViewController <UIScrollViewDelegate>
-{
-  UIScrollView* scrollView;
-}
-
+@interface FirstRunViewController : UIViewController <UIScrollViewDelegate>
 
 @property (nonatomic, retain) IBOutlet UIScrollView* scrollView;
 @property (nonatomic, retain) IBOutlet UIView* bottomView;
 @property (nonatomic, retain) IBOutlet UIView* animView;
-
-- (void)setupBottomView;
-- (UIImage*) maskImage:(UIImage *)image
-              withMask:(UIImage *)maskImage;
-- (void)setupSlide:(UIImageView*)imageView;
 
 @end
