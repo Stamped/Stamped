976e619386bcc8b83a950ec2a1d044fea6b266fc
Andrew Bonventre <andybons@gmail.com>
2011-09-06 11:01:08 -0400

    first run progress.

--- internal/projects/bulleit/Stamped/FirstRunViewController.h
+++ internal/projects/bulleit/Stamped/FirstRunViewController.h
@@ -9,12 +9,23 @@
 #import <QuartzCore/QuartzCore.h>
 #import <UIKit/UIKit.h>
 
-@interface FirstRunViewController : UIViewController <UIScrollViewDelegate>
+@interface FirstRunViewController : UIViewController <UIScrollViewDelegate, UITextFieldDelegate>
 
 @property (nonatomic, retain) IBOutlet UIScrollView* scrollView;
 @property (nonatomic, retain) IBOutlet UIView* bottomView;
-@property (nonatomic, retain) IBOutlet UIView* animView;
+@property (nonatomic, retain) IBOutlet UIView* animationContentView;
 @property (nonatomic, retain) IBOutlet UIButton* signInButton;
 @property (nonatomic, retain) IBOutlet UIButton* createAccountButton;
+@property (nonatomic, retain) IBOutlet UIButton* cancelButton;
+@property (nonatomic, retain) IBOutlet UIButton* confirmButton;
+@property (nonatomic, retain) IBOutlet UIScrollView* signInScrollView;
+@property (nonatomic, retain) IBOutlet UIImageView* stampedLogo;
+@property (nonatomic, retain) IBOutlet UITextField* usernameTextField;
+@property (nonatomic, retain) IBOutlet UITextField* passwordTextField;
+
+- (IBAction)createAccountButtonPressed:(id)sender;
+- (IBAction)signInButtonPressed:(id)sender;
+- (IBAction)cancelButtonPressed:(id)sender;
+- (IBAction)confirmButtonPressed:(id)sender;
 
 @end
