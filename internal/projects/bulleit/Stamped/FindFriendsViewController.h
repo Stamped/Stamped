95af6f92cf98a36498f5294b880c57da755bbf69
Robert Sesek <rsesek@bluestatic.org>
2011-09-10 16:41:59 -0400

    Add FindFriends resources

---
+++ internal/projects/bulleit/Stamped/FindFriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FindFriendsViewController.h
+//  Stamped
+//
+//  Created by Robert Sesek on 9/10/11.
+//  Copyright 2011 Blue Static. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FindFriendsViewController : UIViewController
+
+@end
