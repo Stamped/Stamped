757cb270bf39a6eee331358a72adaaa394dec0a7
Kevin Palms <kevin@stamped.com>
2011-09-24 22:05:09 -0400

    Getting this working..

--- sites/stamped.com/bin/api/HTTPSchemas.py
+++ sites/stamped.com/bin/api/HTTPSchemas.py
@@ -5,7 +5,7 @@ __version__ = "1.0"
 __copyright__ = "Copyright (c) 2011 Stamped.com"
 __license__ = "TODO"
 
-import copy, urllib, urlparse, re, logs
+import copy, urllib, urlparse, re, logs, string
 from datetime import datetime, date
 from schema import *
 from Schemas import *
