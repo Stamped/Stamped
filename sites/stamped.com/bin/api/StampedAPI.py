3eca0564eed9d4b78e8d40b20be2000f9027f0a1
Travis Fischer <travis@stamped.com>
2011-09-27 14:28:53 -0400

    lazily-loaded edetail pages

--- sites/stamped.com/bin/api/StampedAPI.py
+++ sites/stamped.com/bin/api/StampedAPI.py
@@ -630,17 +630,23 @@ class StampedAPI(AStampedAPI):
             entityRequest = entityRequest.value
         entityId    = entityRequest.pop('entity_id', None)
         searchId    = entityRequest.pop('search_id', None)
-
-        if entityId == None and searchId == None:
-            msg = "Required field missing (entityId or search_id)"
+        
+        if entityId is None and searchId is None:
+            msg = "Required field missing (entity_id or search_id)"
             logs.warning(msg)
             raise InputError(msg)
-
-        if searchId != None and searchId.startswith('T_'):
+        
+        if searchId is not None and searchId.startswith('T_'):
             entityId = self._convertSearchId(searchId)
         
         if not entityId:
             entityId = searchId
+        
+        return self._getEntity(entityId)
+    
+    def _getEntity(self, entityId):
+        if entityId is not None and entityId.startswith('T_'):
+            entityId = self._convertSearchId(searchId)
         
         return self._entityDB.getEntity(entityId)
     
