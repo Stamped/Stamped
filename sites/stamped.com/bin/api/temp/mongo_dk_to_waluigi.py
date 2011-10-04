7c37965f03ea1e51fcb4436f5c6e02cf298957cb
Kevin Palms <kevin@stamped.com>
2011-10-04 15:54:30 -0400

    New conversion script (dk_to_waluigi)

---
+++ sites/stamped.com/bin/api/temp/mongo_dk_to_waluigi.py
@@ -0,0 +1,66 @@
+
+import pymongo, json, codecs, os, sys, bson
+from subprocess import Popen, PIPE
+
+OLD_HOST        = "ec2-174-129-76-168.compute-1.amazonaws.com"
+NEW_HOST        = "ec2-184-73-40-130.compute-1.amazonaws.com"
+
+old_connection  = pymongo.Connection(OLD_HOST, 27017)
+new_connection  = pymongo.Connection(NEW_HOST, 27017)
+
+old_database    = old_connection['stamped']
+new_database    = new_connection['stamped']
+
+collections     = old_database.collection_names()
+
+if not os.path.isdir('/stamped/tmp/stamped/'):
+   os.makedirs('/stamped/tmp/stamped')
+
+def main():
+    for collection in collections:
+        print 'RUN %s' % collection
+
+        if collection == 'tempentities':
+            print 'PASS'
+
+        if collection == 'logs':
+            print 'PASS'
+        
+        else:
+            mongoExportImport(collection)
+            print 'COMPLETE'
+
+        print 
+
+
+def mongoExportImport(collection):
+
+    cmdExport = "mongodump --db stamped --collection %s --host %s --out /stamped/tmp" % \
+                (collection, OLD_HOST)
+    cmdImport = "mongorestore --db stamped --collection %s --host %s /stamped/tmp/stamped/%s.bson" % \
+                (collection, NEW_HOST, collection)
+
+    out = open("/stamped/tmp/convert_%s.log" % collection, "w")
+    cmd = "%s && %s && rm -rf /stamped/tmp/stamped/%s.bson" % \
+            (cmdExport, cmdImport, collection)
+    pp  = Popen(cmd, shell=True, stdout=out, stderr=out)
+    pp.wait()
+
+def mongoExportJSON(collection):
+    collection = collection.lower()
+    cmdExport = "mongoexport --db stamped --collection %s --host %s --out /stamped/tmp/stamped/%s.json" % \
+                (collection, OLD_HOST, collection)
+    pp = Popen(cmdExport, shell=True, stdout=PIPE)
+    pp.wait()
+
+def mongoImportJSON(collection):
+    collection = collection.lower()
+    cmdImport = "mongoimport --db stamped --collection %s --host %s /stamped/tmp/stamped/%s_out.json" % \
+                (collection, NEW_HOST, collection)
+    pp = Popen(cmdImport, shell=True, stdout=PIPE)
+    pp.wait()
+    
+
+if __name__ == '__main__':  
+    main()
+
