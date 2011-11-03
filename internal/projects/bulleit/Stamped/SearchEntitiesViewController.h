6ddf495c9fe6fc8406ccc8bf47d462c090775070
Andrew Bonventre <andybons@gmail.com>
2011-11-03 18:23:43 -0400

    finishing up major search revamp

--- internal/projects/bulleit/Stamped/SearchEntitiesViewController.h
+++ internal/projects/bulleit/Stamped/SearchEntitiesViewController.h
@@ -23,17 +23,20 @@ typedef enum {
                                                             UITableViewDataSource>
 
 @property (nonatomic, retain) IBOutlet UITableView* tableView;
+@property (nonatomic, retain) IBOutlet UIImageView* nearbyImageView;
 @property (nonatomic, retain) IBOutlet STSearchField* searchField;
 @property (nonatomic, retain) IBOutlet UIButton* locationButton;
 @property (nonatomic, retain) IBOutlet UITableViewCell* addStampCell;
 @property (nonatomic, retain) IBOutlet UILabel* addStampLabel;
 @property (nonatomic, retain) IBOutlet UITableViewCell* searchingIndicatorCell;
 @property (nonatomic, retain) IBOutlet UILabel* loadingIndicatorLabel;
+@property (nonatomic, retain) IBOutlet UIButton* searchButton;
 
 @property (nonatomic, assign) SearchIntent searchIntent;
 
-- (IBAction)locationButtonTapped:(id)sender;
-- (IBAction)cancelButtonTapped:(id)sender;
+- (IBAction)locationButtonPressed:(id)sender;
+- (IBAction)searchButtonPressed:(id)sender;
+- (IBAction)cancelButtonPressed:(id)sender;
 - (void)resetState;
 
 @end
