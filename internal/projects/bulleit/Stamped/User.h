22517eccb603a0c4458500a2a247dfebe99c6c47
Andrew Bonventre <andybons@gmail.com>
2011-11-07 01:23:56 -0500

    add followers and following to user object.

--- internal/projects/bulleit/Stamped/User.h
+++ internal/projects/bulleit/Stamped/User.h
@@ -28,6 +28,8 @@ extern const CGFloat kMediumUserImageSize;
 @property (nonatomic, retain) NSSet* comments;
 @property (nonatomic, retain) NSSet* events;
 @property (nonatomic, retain) NSSet* credits;
+@property (nonatomic, retain) NSSet* following;
+@property (nonatomic, retain) NSSet* followers;
 @property (nonatomic, retain) NSNumber* numCredits;
 @property (nonatomic, retain) NSNumber* numFollowers;
 @property (nonatomic, retain) NSNumber* numFriends;
