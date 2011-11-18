6d8f25d8dd1df0a142f8ebd28c0cb5ef8c4fb7f1
Kevin Palms <kevin@stamped.com>
2011-11-18 14:58:19 -0500

    Comment out URLs

--- sites/stamped.com/bin/www/urls.py
+++ sites/stamped.com/bin/www/urls.py
@@ -29,11 +29,11 @@ urlpatterns = patterns('',
     url(R'^faq-mobile',                     'teaser.views.mobileFaq'),
 
     # Website
-    url(R'^about$',                         'teaser.views.about'),
-    url(R'^faq$',                           'teaser.views.faq'),
-    url(R'^privacy$',                       'teaser.views.privacy'),
-    url(R'^terms-of-service$',              'teaser.views.terms'),
+    # url(R'^about$',                         'teaser.views.about'),
+    # url(R'^faq$',                           'teaser.views.faq'),
+    # url(R'^privacy$',                       'teaser.views.privacy'),
+    # url(R'^terms-of-service$',              'teaser.views.terms'),
 
     # Index
-    url(R'$',                               'teaser.views.index'), 
+    # url(R'$',                               'teaser.views.index'), 
 )
