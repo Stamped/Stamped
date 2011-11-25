89ede2b08c3e8c3584659f7047246fb8bbf36abf
Andrew Bonventre <andybons@gmail.com>
2011-11-25 14:56:33 -0500

    even more cleanup

--- internal/projects/bulleit/Stamped/AccountManager.m
+++ internal/projects/bulleit/Stamped/AccountManager.m
@@ -186,10 +186,11 @@ static AccountManager* sharedAccountManager_ = nil;
 
   if ([objectLoader.response isUnauthorized] &&
       [objectLoader.resourcePath isEqualToString:kLoginPath]) {
-    if (firstRunViewController_){  
-      [self.firstRunViewController signInFailed:@"The username or password you entered is incorrect."];}    
-    else
+    if (firstRunViewController_) {  
+      [self.firstRunViewController signInFailed:@"The username or password you entered is incorrect."];
+    } else {
       [self performSelector:@selector(logout) withObject:self afterDelay:0];
+    }
   } else if ([objectLoader.resourcePath isEqualToString:kRefreshPath]) {
     [self sendLoginRequest];
   } else if ([objectLoader.resourcePath rangeOfString:kRegisterPath].location != NSNotFound) {
