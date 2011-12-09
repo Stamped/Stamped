537368516a9f7698482972773861881e3c3edd58
Travis Fischer <travis@stamped.com>
2011-12-09 18:10:16 -0500

    .

--- sites/stamped.com/bin/crawler/crawler.py
+++ sites/stamped.com/bin/crawler/crawler.py
@@ -23,7 +23,7 @@ import sources
 
 # import all databases
 import api.db
-from api.MongoStampedAPI import MongoStampedAPI
+from MongoStampedAPI import MongoStampedAPI
 
 #-----------------------------------------------------------
 
