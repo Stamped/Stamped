58244943e7ef528a4d691c4188e51c32d4872b76
Kevin Palms <kevin@stamped.com>
2012-01-05 17:35:22 -0500

    .

--- sites/stamped.com/bin/utils.py
+++ sites/stamped.com/bin/utils.py
@@ -771,6 +771,21 @@ def getFacebook(accessToken, path, params={}):
     
     return result
 
+def getTwitter(url, key, secret, http_method="GET", post_body=None, http_headers=None):
+    consumer = oauth.Consumer(key=TWITTER_CONSUMER_KEY, secret=TWITTER_CONSUMER_SECRET)
+    token = oauth.Token(key=key, secret=secret)
+    client = oauth.Client(consumer, token)
+ 
+    resp, content = client.request(
+        url,
+        method=http_method,
+        body=post_body,
+        headers=http_headers,
+        force_auth_header=True
+    )
+    data = json.loads(content)
+    return data
+
 class HeadRequest(urllib2.Request):
     def get_method(self):
         return "HEAD"
