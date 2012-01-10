a886ca2b4493680275d11e4433e6846861541358
Andrew Bonventre <andybons@gmail.com>
2012-01-10 16:20:52 -0500

    add toolbar subclass for find class

---
+++ clients/ios/Stamped/FindFriendsToolbar.h
@@ -0,0 +1,13 @@
+//
+//  FindFriendsToolbar.h
+//  Stamped
+//
+//  Created by Andrew Bonventre on 1/10/12.
+//  Copyright (c) 2012 Stamped, Inc. All rights reserved.
+//
+
+#import "STToolbar.h"
+
+@interface FindFriendsToolbar : STToolbar
+
+@end
