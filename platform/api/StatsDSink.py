db16d43157ffacc61d6a8f2ff96807e41fa94944
Travis Fischer <travis@stamped.com>
2012-01-11 14:31:46 -0500

    updating copyright to be 2011-2012

--- platform/api/StatsDSink.py
+++ platform/api/StatsDSink.py
@@ -2,7 +2,7 @@
 
 __author__    = "Stamped (dev@stamped.com)"
 __version__   = "1.0"
-__copyright__ = "Copyright (c) 2012 Stamped.com"
+__copyright__ = "Copyright (c) 2011-2012 Stamped.com"
 __license__   = "TODO"
 
 import Globals, utils, logs
