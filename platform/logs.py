5d5f3f27d839ce1cb851e3c7400b353a3e74a8c0
Travis Fischer <travis@stamped.com>
2012-01-18 15:32:28 -0500

    adding logging for async tasks

--- platform/logs.py
+++ platform/logs.py
@@ -105,7 +105,7 @@ def begin(**kwargs):
     saveStat    = kwargs.pop('saveStat', None)
     requestData = kwargs.pop('requestData', None)
     nodeName    = kwargs.pop('nodeName', None)
-
+    
     refresh(format='object')
     
     localData.saveLog  = saveLog
