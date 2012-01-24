8f258bf0c9aa52a7609aba5c5980536e460dc2f7
Travis Fischer <travis@stamped.com>
2012-01-24 18:58:44 -0500

    .

--- platform/api/StampedAPI.py
+++ platform/api/StampedAPI.py
@@ -1505,6 +1505,7 @@ class StampedAPI(AStampedAPI):
     @API_CALL
     def addStampAsync(self, authUserId, stamp_id):
         logs.info("addStampAsync: %s %s" % (authUserId, stamp_id))
+        utils.log("addStampAsync: %s %s" % (authUserId, stamp_id))
         stamp = self._stampDB.getStamp(stamp_id)
         
         # Add references to the stamp in all relevant inboxes
