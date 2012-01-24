09316b712cbb2f27d51c103e8dd0cb78b8a59e2b
Travis Fischer <travis@stamped.com>
2012-01-24 15:52:14 -0500

    adding more integrity checks

--- platform/bin/checkdb.py
+++ platform/bin/checkdb.py
@@ -6,7 +6,7 @@ __copyright__ = "Copyright (c) 2011-2012 Stamped.com"
 __license__   = "TODO"
 
 import Globals
-import integrity, logs, random, time, utils
+import integrity, libs.ec2_utils, logs, random, time, utils
 
 from MongoStampedAPI    import MongoStampedAPI
 from optparse           import OptionParser
