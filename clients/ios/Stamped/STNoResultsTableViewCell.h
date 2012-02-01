4b523a73e0961e9c17d63b16e309e0a36420d708
Andrew Bonventre <andybons@gmail.com>
2012-02-01 18:12:00 -0500

    add "No Results" generic cell for searching

---
+++ clients/ios/Stamped/STNoResultsTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  STNoResultsTableViewCell.h
+//  Stamped
+//
+//  Created by Andrew Bonventre on 2/1/12.
+//  Copyright (c) 2012 Stamped, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STNoResultsTableViewCell : UITableViewCell
+
++ (STNoResultsTableViewCell*)cell;
+
+@end
