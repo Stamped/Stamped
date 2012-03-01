794e62c9d74e98855ac8629be549c861ce183bd8
Travis Fischer <travis@stamped.com>
2012-03-01 01:54:53 -0500

    .

--- platform/bin/test_generic_activity.py
+++ platform/bin/test_generic_activity.py
@@ -19,32 +19,45 @@ request = SuggestedUserRequest(dict(
 ))
 
 api     = MongoStampedAPI()
-users   = api.getSuggestedUsers(user_id, request)
+userDB  = stampedAPI._userDB
 
-for (user, explanations) in users:
-    subject0 = 'Are you interested in '
-    subject1 = user.screen_name
-    subject2 = '\'s tastes?'
-    subject  = subject0 + subject1 + subject2
-    ls0      = len(subject0)
-    indices  = [ ls0, ls0 + len(subject1) ]
-    usermini = user.exportSchema(UserMini())
-    user_id2 = user.user_id
-    blurb    = ' and '.join(explanations)
+rs = userDB._collection.find({'email' : { '$regex' : r'.*@stamped\.com', '$options' : 'i' }}, output=list)
+for result in rs:
+    user = userDB._convertFromMongo(result)
+    user_id = user.user_id
     
-    if blurb:
-        blurb = blurb[0:1].title() + blurb[1:]
+    utils.log('-' * 40)
+    utils.log('%s) %s' % (user.screen_name, user_id))
     
-    utils.log("%s) %s" % (user, explanations))
+    users   = api.getSuggestedUsers(user_id, request)
+
+    for (user, explanations) in users:
+        subject0 = 'Friend suggestion: '
+        subject1 = user.screen_name
+        subject2 = '\'s tastes?'
+        subject  = subject0 + subject1 + subject2
+        ls0      = len(subject0)
+        indices  = [ ls0, ls0 + len(subject1) ]
+        usermini = user.exportSchema(UserMini())
+        user_id2 = user.user_id
+        blurb    = ' and '.join(explanations)
+        
+        if blurb:
+            blurb = blurb[0:1].title() + blurb[1:]
+        
+        utils.log("%s) %s" % (user, explanations))
+        
+        api._addActivity(
+            genre           = 'generic', 
+            user_id         = user_id, # TODO: should this be user_id2? gets overwritten by kwargs anyway..
+            recipient_ids   = [ user_id ], 
+            user            = usermini, 
+            icon            = 'http://static.stamped.com/assets/activity/follower.png', 
+            subject         = subject, 
+            subject_objects = [ ActivityObjectSchema({ 'user_id' : user_id2, 'indices' : indices }) ], 
+            blurb           = blurb, 
+            linked_user_id  = user_id2)
     
-    api._addActivity(
-        genre           = 'generic', 
-        user_id         = user_id, # TODO: should this be user_id2? gets overwritten by kwargs anyway..
-        recipient_ids   = [ user_id ], 
-        user            = usermini, 
-        icon            = 'http://static.stamped.com/assets/activity/follower.png', 
-        subject         = subject, 
-        subject_objects = [ ActivityObjectSchema({ 'user_id' : user_id2, 'indices' : indices }) ], 
-        blurb           = blurb, 
-        linked_user_id  = user_id2)
+    utils.log()
+    utils.log()
 
