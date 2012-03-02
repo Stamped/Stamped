0a9be8271b213278c2bb22ac8e9032874f9afe8a
Travis Fischer <travis@stamped.com>
2012-03-02 18:00:32 -0500

    .

--- platform/bin/StampedElasticMongo.py
+++ platform/bin/StampedElasticMongo.py
@@ -11,10 +11,12 @@ import libs.ec2_utils
 from libs.ElasticMongo      import ElasticMongo
 from api.MongoStampedAPI    import MongoStampedAPI
 
+"""
 import pyes
 es   = pyes.ES('10.85.105.209:9200')
 info = es.collect_info()
 from pprint import pprint; pprint(info)
+"""
 
 if __name__ == '__main__':
     config_ns  = 'local.elasticmongo'
