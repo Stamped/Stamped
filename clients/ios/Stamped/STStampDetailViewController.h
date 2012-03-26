054fc0da28134ec00b841975ac9d92acd0f0224a
Andrew Bonventre <andybons@gmail.com>
2012-03-26 14:14:34 -0400

    new stamp detail class

---
+++ clients/ios/Stamped/STStampDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  STStampDetailViewController.h
+//  Stamped
+//
+//  Created by Andrew Bonventre on 3/26/12.
+//  Copyright (c) 2012 Stamped, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STStampDetailViewController : UIViewController
+
+@end
