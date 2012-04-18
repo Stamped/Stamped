432d8ca6bee0cb21e46ef17600e0cefceb77788f
Kevin Palms <kevin@stamped.com>
2012-04-18 15:24:23 -0400

    .

--- platform/api/db/mongodb/MongoActivityObjectCollection.py
+++ platform/api/db/mongodb/MongoActivityObjectCollection.py
@@ -5,7 +5,7 @@ __version__   = '1.0'
 __copyright__ = 'Copyright (c) 2011-2012 Stamped.com'
 __license__   = 'TODO'
 
-import Globals
+import Globals, utils, logs, pymongo
 
 from AMongoCollection import AMongoCollection
 from Schemas import *
