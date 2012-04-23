387b29931fd8f76e2b1025d57ad694274318b47a
Travis Fischer <travis@stamped.com>
2012-04-23 00:21:52 -0400

    adding base core view code

--- platform/servers/web2/urls.py
+++ platform/servers/web2/urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
 import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Static content
-    (R'^assets/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_DOC_ROOT}),
+    # static assets
+    (R'^assets/(?P<path>.*)$',      'django.views.static.serve', {'document_root': settings.STATIC_DOC_ROOT}), 
     
-    # Index
-    url(R'$', 'teaser.views.index'), 
+    # blog
+    url(R'^blog$',                  'core.views.blog'), 
+    
+    # index
+    url(R'$',                       'core.views.index'), 
 )
 
