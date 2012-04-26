82d87ce73904ea875d8515259e83c5f4b9582e70
Travis Fischer <travis@stamped.com>
2012-04-26 19:17:59 -0400

    finished initial version of stamped web client models

--- platform/servers/web2/assets/js/demo.js
+++ platform/servers/web2/assets/js/demo.js
@@ -16,14 +16,16 @@
             user_model.validate(user_model.attributes);
             
             //alert(user_model.idAttribute());
-            $("#data").append("<pre><code style='font-size: 12px; font-family: \"courier new\" monospace;'>" + JSON.stringify(user_model.toJSON(), null, 4) + "</code></pre>");
+            //$("#data").append("<pre><code style='font-size: 12px; font-family: \"courier new\" monospace;'>" + JSON.stringify(user_model.toJSON(), null, 4) + "</code></pre>");
             
-            /*var stampsP = client.get_user_stamps_by_screen_name(screen_name);
+            var stampsP = client.get_user_stamps_by_screen_name(screen_name);
             stampsP.done(function (stamps) {
                 //console.debug(stamps);
-                stamps = JSON.stringify(stamps, null, 4);
-                $("#data").html("<pre><code style='font-size: 12px; font-family: \"courier new\" monospace;'>" + stamps + "</code></pre>");
-            });*/
+                //stamps = JSON.stringify(stamps, null, 4);
+                var s = new client.Stamps(stamps);
+                
+                $("#data").append("<pre><code style='font-size: 12px; font-family: \"courier new\" monospace;'>" + JSON.stringify(s.toJSON(), null, 4) + "</code></pre>");
+            });
         });
     });
 })();
