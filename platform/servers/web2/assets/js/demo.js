3c738b09461816669eb44a9a21bc0bdf7b53b08c
Travis Fischer <travis@stamped.com>
2012-04-30 13:53:05 -0400

    updates to stamped-web

--- platform/servers/web2/assets/js/demo.js
+++ platform/servers/web2/assets/js/demo.js
@@ -6,20 +6,17 @@
 (function() {
     $(document).ready(function() {
         var client = new StampedClient();
-        var screen_name = document.URL.replace(/^.*\/(\w{1,20})\/?/, "$1");
+        var screen_name = STAMPED_PRELOAD.user.screen_name;
         console.debug("Stamped profile page for screen_name '" + screen_name + "'");
         
+        return;
+        
         var userP = client.get_user_by_screen_name(screen_name);
+        
         userP.done(function (user) {
-            log(user.get('user_id'));
-            //$("#data").append("<pre><code style='font-size: 12px; font-family: \"courier new\" monospace;'>" + JSON.stringify(user.toJSON(), null, 4) + "</code></pre>");
-            
             var stampsP = client.get_user_stamps_by_screen_name(screen_name);
+            
             stampsP.done(function (stamps) {
-                //console.debug(stamps);
-                //stamps = JSON.stringify(stamps, null, 4);
-                //$("#data").append("<pre><code style='font-size: 12px; font-family: \"courier new\" monospace;'>" + JSON.stringify(stamps.toJSON(), null, 4) + "</code></pre>");
-                
                 $("#data2").hide();
                 var stamps_view = new client.StampsGalleryView({
                     model : stamps, 
