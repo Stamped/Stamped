f710f6daee64619c2c761e314584720dce733daa
Michael Lowin <mike@stamped.com>
2012-05-04 11:27:49 -0400

    .

--- platform/api/HTTPSchemas.py
+++ platform/api/HTTPSchemas.py
@@ -1579,6 +1579,8 @@ class HTTPEntityMini(Schema):
             self.category           = schema.category
             self.subcategory        = schema.subcategory
             self.images             = schema.images
+            from pprint import pformat
+            logs.info(pformat('schema.images:\n%s\nself.images:\n%s' % (schema.images, self.images)))
 
             try:
                 if 'coordinates' in schema.value:
