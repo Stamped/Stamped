5a5bbf2afc95a7fbb086760c2e50cdbb447ee34f
Michael Lowin <mike@stamped.com>
2012-05-04 15:08:14 -0400

    .

--- platform/api/StampedAPI.py
+++ platform/api/StampedAPI.py
@@ -773,7 +773,7 @@ class StampedAPI(AStampedAPI):
 
         # TODO return HTTPAction to invoke sign in if credentials are unavailable
 
-        logs.info('netflix_user_id: %s    netflix_token: %s   netflix_secret: %s'
+        logs.info('netflix_user_id: %s    netflix_token: %s   netflix_secret: %s' %
                     (account.netflix_user_id, account.netflix_token, account.netflix_secret))
 
         if account.netflix_user_id == None or account.netflix_token == None or account.netflix_secret != None:
