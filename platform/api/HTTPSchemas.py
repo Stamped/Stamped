80fd2cdb96af0e1d182d6616c5efa1c891987e3c
Michael Lowin <mike@stamped.com>
2012-05-09 17:07:20 -0400

    fixing galleries in httpschemas, I hope

--- platform/api/HTTPSchemas.py
+++ platform/api/HTTPSchemas.py
@@ -1414,7 +1414,7 @@ class HTTPEntity(Schema):
                 gallery = HTTPEntityGallery()
                 gallery.layout = 'list'
                 for album in entity.albums:
-                    print('\nalbum loop')
+                    info.logs('\nalbum loop')
                     try:
                         item            = HTTPImageSchema()
                         size            = HTTPImageSizeSchema()
@@ -1441,7 +1441,7 @@ class HTTPEntity(Schema):
                         logs.info(e.message)
                         pass
                 if len(gallery.images) > 0:
-                    print('\nadding gallery')
+                    info.logs('\nadding gallery with %d images' % len(gallery.images))
                     self.galleries.append(gallery)
 
         elif entity.kind == 'software' and entity.isType('app'):
