2fdb7dc271b9aa644c1dc3d3122325a396e00037
Michael Lowin <mike@stamped.com>
2012-05-11 12:05:41 -0400

    fixing galleries in httpschemas, I hope

--- platform/libs/Memcache.py
+++ platform/libs/Memcache.py
@@ -85,7 +85,7 @@ class Memcache(object):
     
     def _import_value(self, value):
         """
-            returns a custom, pickleable version of the given value for storage 
+            returns a custom, pickleable version of the given value for storage
             within memcached.
         """
         
@@ -97,7 +97,9 @@ class Memcache(object):
         elif isinstance(value, (list, tuple)):
             value = map(self._import_value, value)
         elif isinstance(value, dict):
+            from pprint import pformat
             logs.info('\n### memcache _import_value about to hit lambda')
+            logs.info(pformat(value))
             value = dict(map(lambda k, v: (k, self._import_value(v)), value.iteritems()))
         
         return value
