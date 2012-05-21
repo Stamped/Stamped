fa5b7823078da0b156743ebdede132e61c610ecc
Michael Lowin <mike@stamped.com>
2012-05-21 16:16:34 -0400

    .

--- platform/api/Entity.py
+++ platform/api/Entity.py
@@ -480,7 +480,7 @@ def upgradeEntityData(entityData):
 
         setBasicGroup(place, new, 'address', 'formatted_address')
         if 'hours' in place:
-            place['hours'] = HoursSchema().dataImport(place['hours'].dataExport(), overflow=True)
+            place['hours'] = HoursSchema().dataImport(place['hours'], overflow=True)
         setBasicGroup(place, new, 'hours', seed=False)
         setBasicGroup(restaurant, new, 'menu', seed=False)
         setBasicGroup(restaurant, new, 'price_range', seed=False)
