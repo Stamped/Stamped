ab51c84d2051bca5e7d62427d9b5ff868c2df2bc
Michael Lowin <mike@stamped.com>
2012-05-22 13:56:59 -0400

    .

--- platform/api/StampedAPI.py
+++ platform/api/StampedAPI.py
@@ -1266,7 +1266,8 @@ class StampedAPI(AStampedAPI):
             entityId = self._convertSearchId(entityId)
         else:
             self.mergeEntityId(entityId)
-        
+
+        logs.info('### about to call entityDB.getEntity')
         return self._entityDB.getEntity(entityId)
     
     @API_CALL
@@ -1279,6 +1280,7 @@ class StampedAPI(AStampedAPI):
     
     @API_CALL
     def getEntity(self, entityRequest, authUserId=None):
+        logs.info('### calling stampedapi.getEntity')
         entity = self._getEntityFromRequest(entityRequest)
         
         if self.__version > 0 and entity.isType('artist'):
