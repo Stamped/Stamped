17473a48713e758bb4c1c9acb51a025bfab988a0
Kevin Palms <kevin@stamped.com>
2012-05-27 16:24:50 -0400

    .

--- platform/servers/httpapi/urls.py
+++ platform/servers/httpapi/urls.py
@@ -127,6 +127,7 @@ urlpatterns = patterns('',
     (r'v0/stamps/remove.json',                      'v0.functions.stamps.remove'),
     (r'v0/stamps/collection.json',                  'v0.functions.stamps.collection'),
     (r'v0/stamps/search.json',                      'v0.functions.stamps.search'),
+    (r'v0/stamps/guide.json',                       'v0.functions.stamps.guide'),
     
     (r'v0/stamps/likes/create.json',                'v0.functions.stamps.likesCreate'),
     (r'v0/stamps/likes/remove.json',                'v0.functions.stamps.likesRemove'),
