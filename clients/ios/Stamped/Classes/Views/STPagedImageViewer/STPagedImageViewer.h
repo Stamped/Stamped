607925f0955bebeed4ca63b8392d53ea9266b843
Devin Doty <devin.r.doty@gmail.com>
2012-05-29 20:25:23 -0400

    stamp cell updates

--- clients/ios/Stamped/Classes/Views/STPagedImageViewer/STPagedImageViewer.h
+++ clients/ios/Stamped/Classes/Views/STPagedImageViewer/STPagedImageViewer.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
+@class STPageControl;
 @interface STPagedImageViewer : UIView {
     NSInteger _currentPage;
     NSArray *_views;
@@ -16,7 +17,7 @@
 
 @property (nonatomic,strong) NSArray *imageURLs;
 @property (nonatomic,strong,readonly) UIScrollView *scrollView;
-@property (nonatomic,strong,readonly) UIPageControl *pageControl;
+@property (nonatomic,strong,readonly) STPageControl *pageControl;
 
 - (void)moveToPage:(NSInteger)page animated:(BOOL)animated;
 
