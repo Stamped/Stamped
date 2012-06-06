8913ed6517432927a8c74d3758b961827e12cae8
Michael Lowin <mike@stamped.com>
2012-06-06 16:45:35 -0400

    -fixing activity behavior so there's no overlap between personal and friends feeds -more Activity unit tests

--- platform/api/StampedAPI.py
+++ platform/api/StampedAPI.py
@@ -4059,9 +4059,33 @@ class StampedAPI(AStampedAPI):
             friends = self._friendshipDB.getFriends(authUserId)
             activityData = []
             params['verbs'] = ['comment', 'like', 'todo', 'restamp', 'follow']
+
             dirtyActivityData = self._activityDB.getActivityForUsers(friends, **params)
+            activityItemIds = [item.activity_id for item in dirtyActivityData]
+            # Find which activity items with friends in subject are already appearing in our Personal Activity feed
+            personalActivityIds = self._activityDB.getActivityIdsForUser(authUserId, **params)
+            overlappingActivityIds =  list(set(personalActivityIds).intersection(set(activityItemIds)))
+
+            from pprint import pformat
+            logs.info('###### overlapping activitydata')
+            logs.info(pformat(overlappingActivityIds))
+
             for item in dirtyActivityData:
+                # Exclude the item if it is in the user's personal feed, unless it is a 'follow' item.  In that case,
+                #  remove the user as an object and ensure there are still other users targeted by the item
+                isInPersonalFeed = item.activity_id in overlappingActivityIds
+                if isInPersonalFeed and item.verb in ['comment', 'like',  'todo', 'restamp' ]:
+                    continue
+                elif isInPersonalFeed and item.verb in ['follow']:
+                    assert(item.objects is not None and authUserId in item.objects.user_ids)
+                    newUserIds = list(item.objects.user_ids)
+                    newUserIds.remove(authUserId)
+                    item.objects.user_ids = newUserIds
+                    if len(item.objects.user_ids) == 0:
+                        continue
+
                 item.subjects = list(set(item.subjects).intersection(set(friends)))
+                assert(len(item.subjects) > 0)
                 activityData.append(item)
         else:
             personal = True
