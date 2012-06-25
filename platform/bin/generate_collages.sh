d6140c0ba66bb1a2757a7855670f9711e286f50a
Travis Fischer <travis@stamped.com>
2012-06-25 03:26:09 -0400

    .

--- platform/bin/generate_collages.sh
+++ platform/bin/generate_collages.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-python collage.py --s3 --db peach.db2 -U travis -U kelly -U bart -U robby -U edmuki -U anthony -U lizwalton -U paul -U landon -U kevin -U parislemon -U mariobatali -U danielle -U jenny -U lisa -U mike -U parislemon -U hoogs -U brettgarrett -U davidfraga -U startupman -U kevinsystrom -U lindsay -U matt -U noah -U richminer -U sahil -U smanson22 -U slazar -U julie -U ted -U susan -U yancius -U ilyse -U tconrad
-
-#andy
+python collage.py --s3 --db peach.db2 -U travis -U kelly -U bart -U robby -U edmuki -U anthony -U lizwalton -U paul -U landon -U kevin -U parislemon -U mariobatali -U julia -U lisa -U brettgarrett -U davidfraga -U andy -U sfriend37 -U slazar -U matt -U heather -U tristan -U jenny -U snambomb -U jarvis -U mike -U danielle -U startupman -U julie -U ted -U susan -U bsbernstein -U parislemon -U hoogs -U kevinsystrom -U lindsay -U pkdub -U noah -U richminer -U sahil -U smanson22 -U yancius -U tconrad -U alexmarkman -U rooklin -U johnca -U hearjt -U mirandalevitt -U annar08 -U abl64 -U kerrita -U weston -U elliotlum -U taps
 
