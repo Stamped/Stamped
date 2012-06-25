c44c241f565f6cb85ed0e7567a21fb0f4b9c067f
Joey <joey@stamped.com>
2012-06-25 12:07:04 -0400

    New dashboard for web

--- platform/analytics/Dashboard.py
+++ platform/analytics/Dashboard.py
@@ -30,17 +30,47 @@ conn = SDBConnection(keys.aws.AWS_ACCESS_KEY_ID, keys.aws.AWS_SECRET_KEY)
 
 v1_init = datetime(2011,11,21)
 
+def now():
+    return datetime.today()
+
 def today():
-    now =  datetime.today()
+    now = datetime.today()
     diff = timedelta(days=0,hours=now.hour,minutes=now.minute,seconds=now.second,microseconds=now.microsecond)
     return now - diff
 
+def yesterday(date):
+    return date - timedelta(days=1)
 
-def totalStamps(): 
-    total = stamp_collection.count()
-    new = stamp_collection.find({'timestamp.created': {'$gte': today()}}).count()
-    delta = float(new) / (total - new) *100.0
-    return (total,new,delta)
+def weekAgo(date):
+    return date - timedelta(days=6)
+
+def totalStamps():
+    todays = stamp_collection.find({'timestamp.created': {'$gte': today()}}).count()
+    yesterdays = stamp_collection.find({'timestamp.created': {'$gte': yesterday(today()), '$lt': yesterday(now())}}).count()
+    
+    
+    weeklyAgg = yesterdays
+    bgn = yesterday(today())
+    end = yesterday(now())
+    for i in range (0,5):
+        bgn = yesterday(bgn)
+        end = yesterday(end)
+        weeklyAgg += stamp_collection.find({'timestamp.created': {'$gte': bgn, '$lt': end}}).count()
+        
+    weeklyAvg = weeklyAgg / 6.0
+    
+    try:
+        deltaDay = float(todays - yesterdays)/(yesterdays)*100.0
+    except ZeroDivisionError:
+        deltaDay = 'N/A'
+    
+    try: 
+        deltaWeek = float(todays - weeklyAvg)/(weeklyAvg)*100
+    except ZeroDivisionError:
+        deltaWeek = 'N/A'
+    
+    
+    return (now(),todays,yesterdays,weeklyAvg,deltaDay,deltaWeek)
 
 def totalAccounts():
     total = acct_collection.count()
@@ -56,33 +86,37 @@ def todaysUsers():
     
 while True:
     
-    totalS,newS,deltaS = totalStamps()
-    print "Total Stamps: %s" % totalS
-    print "New Stamps Today: %s" % newS
-    print "Change: %s%%\n\n" % deltaS
+    hour,todays,yesterdays,weeklyAvg,deltaDay,deltaWeek = totalStamps()
     
-    totalA,newA,deltaA = totalAccounts()
-    print "Total Accounts: %s" % totalA
-    print "New Accounts Today: %s" % newA
-    print "Change: %s%%\n\n" % deltaA
+    print hour
+    print "Stamps Today: %s" % todays
+    print "Stamps Yesterday: %s" % yesterdays
+    print "D2D Change: %s%%" % deltaDay
+    print "Weekly Avg: %s" % weeklyAvg
+    print "W2D Change: %s%%" % deltaWeek
     
-    users = todaysUsers()
-    print "Unique users today: %s\n\n" % users
-   
-    print "Most Stamped Today:"
-    dailyTop = getTopStamped(None,str(today().date()),stamp_collection)
-    count = 1
-    for i in dailyTop[0:25]:
-        print "%s) %s: %s stamps" % (count, i['_id'], int(i['value']))
-        count += 1
-    
-    print "\nTrending this Week:"
-    past_week = today() - timedelta(days=6)
-    weeklyTop = getTopStamped(None,str(past_week.date()),stamp_collection)
-    count = 1
-    for i in weeklyTop[0:25]:
-        print "%s) %s" % (count, i['_id'])
-        count += 1
+#    totalA,newA,deltaA = totalAccounts()
+#    print "Total Accounts: %s" % totalA
+#    print "New Accounts Today: %s" % newA
+#    print "Change: %s%%\n\n" % deltaA
+#    
+#    users = todaysUsers()
+#    print "Unique users today: %s\n\n" % users
+#   
+#    print "Most Stamped Today:"
+#    dailyTop = getTopStamped(None,str(today().date()),stamp_collection)
+#    count = 1
+#    for i in dailyTop[0:25]:
+#        print "%s) %s: %s stamps" % (count, i['_id'], int(i['value']))
+#        count += 1
+#    
+#    print "\nTrending this Week:"
+#    past_week = today() - timedelta(days=6)
+#    weeklyTop = getTopStamped(None,str(past_week.date()),stamp_collection)
+#    count = 1
+#    for i in weeklyTop[0:25]:
+#        print "%s) %s" % (count, i['_id'])
+#        count += 1
     
     #Show me the top stamped this week by vertical
     
