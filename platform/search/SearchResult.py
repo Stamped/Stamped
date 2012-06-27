43e9d41b45a182f5492123104299148ce60887e6
Paul Eastlund <paul@stamped.com>
2012-06-26 23:26:56 -0400

    Fix some unicode bullshit.

--- platform/search/SearchResult.py
+++ platform/search/SearchResult.py
@@ -19,7 +19,9 @@ class SearchResult(object):
         self.resolverObject = resolverObject
 
     def addScoreComponentDebugInfo(self, componentName, componentValue):
-        self.__scoreDebugInfo.append((normalize(componentName), componentValue))
+        if isinstance(componentName, unicode):
+            componentName = componentName.encode('utf-8')
+        self.__scoreDebugInfo.append((componentName, componentValue))
 
     @property
     def scoreDebugInfo(self):
@@ -30,7 +32,6 @@ class SearchResult(object):
         return self
 
     def __repr__(self):
-        scoreDetails = ''
-        # scoreDetails = '\n'.join(['  %s: %f' % componentInfo for componentInfo in self.__scoreDebugInfo])
+        scoreDetails = '\n'.join(['  %s: %f' % componentInfo for componentInfo in self.__scoreDebugInfo])
         return 'Score: %f\nSource: %s\nScore details: %s\nResult:\n%s' % \
                (self.score, self.resolverObject.source, scoreDetails, indentText(str(self.resolverObject), 4))
