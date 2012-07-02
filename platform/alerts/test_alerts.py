e60af7fa79b2aedacf4bdeac9fbaaf1e119784a6
Kevin Palms <kevin@stamped.com>
2012-07-02 18:29:15 -0400

    Updates to APNS

--- platform/alerts/test_alerts.py
+++ platform/alerts/test_alerts.py
@@ -14,6 +14,8 @@ host_name = 'gateway.sandbox.push.apple.com'
 #deviceId = '1c8cf15acb17f8362322ccff0452417dcd3f6b538193099e0347efb84e8a4a4f' # kevin
 #deviceId = 'f02e7b4c384e32404645443203dd0b71750e54fe13b5d0a8a434a12a0f5e7a25' # bart
 deviceId = '8b78c702f8c8d5e02c925146d07c28f615283bc862b226343f013b5f8765ba5a' # travis
+deviceId = '9d943f189085639bd87bac6c9dd2b89f237bee1e232041d4057f7a422f23deeb' #landon
+deviceId = 'df687e03345604f6b02a4c32bc7d5220ddd5f832c270645e06f22cc26f66516a' # Kevin
 
 # Build payload
 content = {
@@ -34,7 +36,7 @@ payload = struct.pack(fmt, command, 32, binascii.unhexlify(deviceId), \
 s = socket()
 c = ssl.wrap_socket(s, 
                     ssl_version=ssl.PROTOCOL_SSLv3, 
-                    certfile=IPHONE_APN_PUSH_CERT_DEV)
+                    certfile=IPHONE_APN_PUSH_CERT_PROD)
 c.connect((host_name, 2195))
 c.write(payload)
 c.close()
