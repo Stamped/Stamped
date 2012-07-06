150e6f7fd9cbb3828f2c55c45ce9009f9ac2246a
Kevin Palms <kevin@stamped.com>
2012-07-06 11:56:33 -0400

    Test analytics server in nginx config

--- platform/servers/httpapi/v0/helpers.py
+++ platform/servers/httpapi/v0/helpers.py
@@ -395,7 +395,7 @@ def checkOAuth(oauth_token, required=True):
         raise
     except Exception, e:
         logs.warning("Error: %s" % e)
-        raise StampedAuthError("invalid_token", "Invalid access token")
+        raise StampedHTTPError(401, "invalid_token", "Invalid access token")
 
 def parseRequest(schema, request, **kwargs):
     data = { }
