3eacb98b525a0dc1d088fa15e0355ae44fd809af
Kevin Palms <kevin@stamped.com>
2012-07-07 17:30:52 -0400

    .

--- platform/alerts/alerts.py
+++ platform/alerts/alerts.py
@@ -531,7 +531,7 @@ class NotificationQueue(object):
 
             finally:
                 if not noop:
-                    inviteDB.removeInvite(invite.invite_id)
+                    self._inviteDB.removeInvite(invite.invite_id)
 
     def sendEmails(self, noop=False):
         logs.info("Submitting emails to %s users" % len(self._emailQueue))
