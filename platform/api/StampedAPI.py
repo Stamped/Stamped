528b77aff9105c7315a77fa49e7b1689326ce3e4
Paul Eastlund <paul@stamped.com>
2012-07-10 14:31:23 -0400

    .

--- platform/api/StampedAPI.py
+++ platform/api/StampedAPI.py
@@ -4390,7 +4390,7 @@ class StampedAPI(AStampedAPI):
         sourceAndKeyRe = re.compile('^([A-Z]+)_([\w+-:/]+)$')
         sourcesAndKeys = []
         for component in id_components:
-            match = sourceAndKey Re.match(component)
+            match = sourceAndKeyRe.match(component)
             if not match:
                 logs.warning('Unable to parse search ID component:' + component)
             else:
