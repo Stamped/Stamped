c6939719391de356c7c465ce2c2520a0c7842abf
Robby Stein <robby@stamped.com>
2012-07-17 15:47:35 -0400

    error msgs

--- platform/servers/httpapi/v0/functions/account.py
+++ platform/servers/httpapi/v0/functions/account.py
@@ -16,14 +16,14 @@ from libs.Facebook           import *
 
 accountExceptions = [
     (StampedInvalidEmailError,              400, 'invalid_credentials', "Invalid email address"),
-    (StampedInvalidScreenNameError,         400, 'invalid_credentials', "Invalid screen name"),
-    (StampedScreenNameInUseError,           400, 'invalid_credentials', "Screen name is already in use"),
-    (StampedBlackListedScreenNameError,     403, 'forbidden',           'Invalid screen name'),
+    (StampedInvalidScreenNameError,         400, 'invalid_credentials', "Invalid username"),
+    (StampedScreenNameInUseError,           400, 'invalid_credentials', "Username is already in use"),
+    (StampedBlackListedScreenNameError,     403, 'forbidden',           'Invalid username'),
     (StampedInvalidPasswordError,           403, 'invalid_credentials', 'Incorrect password'),
     (StampedInvalidWebsiteError,            403, 'invalid_credentials', "Could not update account website"),
     (StampedInvalidStampColorsError,        403, 'invalid_credentials', "Invalid stamp colors"),
     (StampedDuplicateEmailError,            409, 'invalid_credentials', "An account already exists with that email address"),
-    (StampedDuplicateScreenNameError,       409, 'invalid_credentials', "An account already exists with that screen name"),
+    (StampedDuplicateScreenNameError,       409, 'invalid_credentials', "An account already exists with that username"),
     (StampedAccountNotFoundError,           404, 'not_found',           'There was an error retrieving account information'),
     (StampedAlreadyStampedAuthError,        400, 'bad_request',         'This account is already a Stamped account'),
     (StampedLinkedAccountMismatchError,     400, 'illegal_action',      "There was a problem verifying the third-party account"),
