a420f5724b41a96c6fcf242c636b2197f0629331
Geoff Liu <geoff@stamped.com>
2012-07-21 13:41:08 -0400

    make fastcompare_setup.py use absolute path

--- platform/resolve/fastcompare_setup.py
+++ platform/resolve/fastcompare_setup.py
@@ -6,10 +6,12 @@ __copyright__ = "Copyright (c) 2011-2012 Stamped.com"
 __license__   = "TODO"
 
 import Globals
+import inspect, os
 from utils import is_ec2
 from distutils.core import setup, Extension
 
 debug_macro = [] if is_ec2() else [('NDEBUG', '1')]
-fast_compare_module = Extension('fastcompare', define_macros=debug_macro, sources=['platform/resolve/fastcompare.c'])
+script_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+fast_compare_module = Extension('fastcompare', define_macros=debug_macro, sources=[os.path.join(script_dir, 'fastcompare.c')])
 
 setup(name="FastCompare", ext_modules=[fast_compare_module])
