56024bf1ba708fb94050e44654d4df368bb2bb24
Michael Lowin <mike@stamped.com>
2012-07-24 12:31:29 -0400

    .

--- platform/api/db/mongodb/MongoCollectionProxy.py
+++ platform/api/db/mongodb/MongoCollectionProxy.py
@@ -43,7 +43,6 @@ class MongoCollectionProxy(object):
         
         while True:
             try:
-                print ('### Cursor: %s' % self._collection.find(spec, **kwargs).explain()['cursor'])
                 ret = self._collection.find(spec, **kwargs)
                 
                 if limit is not None:
