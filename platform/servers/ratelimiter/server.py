5b9158451e316c9c1e25ddf48ee581cf55453ce7
Michael Lowin <mike@stamped.com>
2012-07-25 15:09:52 -0400

    .

--- platform/servers/ratelimiter/server.py
+++ platform/servers/ratelimiter/server.py
@@ -44,8 +44,8 @@ class StampedRateLimiterRPCService(rpyc.Service):
         pass
 
     def exposed_request(self, service, priority, timeout, verb, url, body = None, headers = None):
-        logs.info('Received request.  service: %s  priority: %s  timeout: %s  verb: %s  url: %s  body: %s  headers: %s' %
-                  (service, priority, timeout, verb, url, body, headers))
+        logs.info('Received request.  service: %s  priority: %s  timeout: %s  verb: %s  url: %s  ' %
+                  (service, priority, timeout, verb, url))
         if body is not None:
             body = pickle.loads(body)
         if headers is not None:
