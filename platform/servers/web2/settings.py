d0e0635fe5dd942ceb463e0e928574aa744d7479
Travis Fischer <travis@stamped.com>
2012-07-26 08:29:10 -0400

    .

--- platform/servers/web2/settings.py
+++ platform/servers/web2/settings.py
@@ -177,14 +177,14 @@ LOGGING = {
     'disable_existing_loggers': False,
     'handlers': {
         'mail_admins': {
-            'level': 'ERROR',
+            'level': 'CRITICAL',
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
-            'level': 'ERROR',
+            'level': 'CRITICAL',
             'propagate': True,
         },
     }
