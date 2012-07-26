bb6a948b25db7c74823953b55d9a2d0e82348339
Joey <jws295@cornell.edu>
2012-07-26 15:19:20 -0400

    Update to anlaytics, new sdb logger

--- platform/servers/analytics/settings.py
+++ platform/servers/analytics/settings.py
@@ -78,14 +78,14 @@ if IS_PROD:
 else:
     SITE_ROOT   = PROJ_ROOT
 
-STATIC_DOC_ROOT = SITE_ROOT
+STATIC_DOC_ROOT = os.path.join(SITE_ROOT, 'assets')
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(STATIC_DOC_ROOT, 'assets'), 
+    STATIC_DOC_ROOT, 
 )
 
 # List of finder classes that know how to find static files in
