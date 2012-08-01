2ae641567ed241abd032399a87f1ca2d7d797179
mikelow <mike@stamped.com>
2012-08-01 00:37:54 -0400

    enabling follow activity

--- platform/api/StampedAPI.py
+++ platform/api/StampedAPI.py
@@ -1474,7 +1474,7 @@ class StampedAPI(AStampedAPI):
                         'authUserId': authUserId,
                         'followUserId': userId,
                     }
-                    # self.callTask(self.postToOpenGraphAsync, payload)
+                    self.callTask(self.postToOpenGraphAsync, payload)
 
     @API_CALL
     def removeFriendship(self, authUserId, userRequest):
