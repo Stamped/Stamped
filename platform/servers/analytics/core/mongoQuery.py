8914119bbbfedf6f41a043acee55e191bb894f94
Joey <jws295@cornell.edu>
2012-08-07 16:02:32 -0400

    .

--- platform/servers/analytics/core/mongoQuery.py
+++ platform/servers/analytics/core/mongoQuery.py
@@ -94,26 +94,26 @@ class mongoQuery(object):
         if version not in ["v1","v2"]:
             return 0
         # All users following at least one person who is not a suggested user
-        users = self.api._friendshipDB.friends_collection.find({'ref_ids': {'$elemMatch': {'$nin': ['4ff72c599713965571000984',
-                                                                                                       '4ff2279797139655710004bb',
-                                                                                                       '4ff72cd097139667e50001c8',
-                                                                                                       '4ff72d3a9713960355000509',
-                                                                                                       '4f590e37b951fe37730005ad',
-                                                                                                       '4ff48142d56d834b79000744',
-                                                                                                       '4ff47ff0971396601500057c',
-                                                                                                       '4e972d8dfe4a1d22d30002d1',
-                                                                                                       '4e9b41f0fe4a1d44dd000601',
-                                                                                                       '4ecc1f01e8ef215a42000339',
-                                                                                                       '4e792021d6970356a5000042',
-                                                                                                       '5011d981c5fc3e11461b6ee8',
-                                                                                                       '500f3833d56d83787e000647',
-                                                                                                       '4fc107f2b951fe2f75000d36',
-                                                                                                       '4f567cdcd56d833b66000165',
-                                                                                                       '4ecaedd2366b3c15540004f0',
-                                                                                                       '4fe3721b9713967a5000007e',
-                                                                                                       '4e8dfef6967ed34b3e00086c',
-                                                                                                       '4e985cc7fe4a1d2fc4000220',
-                                                                                                       '4f5279b5591fa45c3700053b']}}})
+        users = self.api._friendshipDB.friends_collection._collection.find({'ref_ids': {'$elemMatch': {'$nin': ['4ff72c599713965571000984',
+                                                                                                               '4ff2279797139655710004bb',
+                                                                                                               '4ff72cd097139667e50001c8',
+                                                                                                               '4ff72d3a9713960355000509',
+                                                                                                               '4f590e37b951fe37730005ad',
+                                                                                                               '4ff48142d56d834b79000744',
+                                                                                                               '4ff47ff0971396601500057c',
+                                                                                                               '4e972d8dfe4a1d22d30002d1',
+                                                                                                               '4e9b41f0fe4a1d44dd000601',
+                                                                                                               '4ecc1f01e8ef215a42000339',
+                                                                                                               '4e792021d6970356a5000042',
+                                                                                                               '5011d981c5fc3e11461b6ee8',
+                                                                                                               '500f3833d56d83787e000647',
+                                                                                                               '4fc107f2b951fe2f75000d36',
+                                                                                                               '4f567cdcd56d833b66000165',
+                                                                                                               '4ecaedd2366b3c15540004f0',
+                                                                                                               '4fe3721b9713967a5000007e',
+                                                                                                               '4e8dfef6967ed34b3e00086c',
+                                                                                                               '4e985cc7fe4a1d2fc4000220',
+                                                                                                               '4f5279b5591fa45c3700053b']}}})
         user_ids = map(lambda x: ObjectId(x['_id']), users)
         
         if version == "v1":
