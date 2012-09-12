118d3c3576fbd89d7e97a72203cf25ef4273935b
Michael Lowin <mike@stamped.com>
2012-09-12 16:26:42 -0400

    more instagram tweaks

--- platform/libs/Instagram.py
+++ platform/libs/Instagram.py
@@ -91,7 +91,8 @@ class Instagram(object):
 
 
     def createInstagramImage(self, entity_img_url, stamp_url, profile_img_url, user_generated, coordinates,
-                             primary_color, secondary_color, user_name, category, types, title, subtitle):
+                             primary_color, secondary_color, user_name, category, types, title, subtitle,
+                             a,b,c,d,e,f,g,h):
         def dropShadow(rounded, size, background=0xffffff, shadow=0x444444, border=8, iterations=3):
             """
             Create a gaussian blur drop shadow
@@ -143,6 +144,13 @@ class Instagram(object):
                     pix = pixdata[x, y]
                     pixdata[x, y] = (0, 0, 0, 255-pix[0])
 
+        def white_to_transparent(img):
+            pixdata = img.load()
+
+            for y in xrange(img.size[1]):
+                for x in xrange(img.size[0]):
+                    pix = pixdata[x, y]
+                    pixdata[x, y] = (0, 0, 0, pix[0])
 
         def invert_to_transparent(img):
             pixdata = img.load()
@@ -151,9 +159,9 @@ class Instagram(object):
                 for x in xrange(img.size[0]):
                     pix = pixdata[x, y]
                     if pix == (255, 255, 255, 255):
-                        pixdata[x,y] = (255, 255, 255, 0)
+                        pixdata[x,y] = (0, 0, 0, 0)
                     else:
-                        pixdata[x,y] = (255, 255, 255, 255)
+                        pixdata[x,y] = (0, 0, 0, 255-pix[0])
 
 
         def fill_img(entityImg):
@@ -168,7 +176,8 @@ class Instagram(object):
             img = Image.new('RGBA', (m, m), (255,255,255,128))
             entityImg.paste(img, img)
             entityImg = entityImg.resize((612, 612))
-            entityImg = entityImg.filter(ImageFilter.BLUR)
+            for i in range(3):
+                entityImg = entityImg.filter(ImageFilter.BLUR)
 
             return entityImg
 
@@ -230,8 +239,11 @@ class Instagram(object):
 
             return entityImg
 
-        def getCategoryIcon(category):
-            categoryIcons = Image.open(self.__basepath + 'categoryicons18px.png')
+        def getCategoryIcon(category, light=True):
+            if light:
+                categoryIcons = Image.open(self.__basepath + 'categoryicons-light.png')
+            else:
+                categoryIcons = Image.open(self.__basepath + 'categoryicons-dark.png')
             categories = ('restaurant', 'bar', 'cafe', 'place', 'album', 'song', 'artist', 'film', 'tv_show',
                           'book', 'software', 'other')
             if category not in categories:
@@ -240,11 +252,11 @@ class Instagram(object):
                 categoryIndex = categories.index(category)
             print categoryIndex
 
-            icon = ImageChops.offset(categoryIcons, -28*categoryIndex, 0)
+            icon = ImageChops.offset(categoryIcons, -(30*categoryIndex)+2, 0)
             icon = icon.crop((0, 0, 28, 28))
             return icon
 
-        def getInstagramTextImg(user_name, category, types, title, subtitle, stamp=None, color=None, rule='dark'):
+        def getInstagramTextImg(user_name, category, types, title, subtitle, stamp=None, light=True):
             textImg = Image.new('RGBA', (612*2,195*2), (255,255,255,0))
             draw = ImageDraw.Draw(textImg)
             titling_gothic = ImageFont.truetype(self.__basepath +"TitlingGothicFB.ttf", 80*2)
@@ -278,38 +290,44 @@ class Instagram(object):
                 break
             subtitleW, subtitleH = draw.textsize(subtitle, font=helvetica_neue)
 
-            if color is not None:
-                fill_clr = color
-                title_clr = color
-            else:
+            if light:
                 fill_clr = '#939393'
                 title_clr = '#000000'
+            else:
+                fill_clr = '#000000'
+                title_clr = '#000000'
 
-            draw.text((612-((headerW+header_nameW)/2),0), user_name, font=helvetica_neue_bold, fill=fill_clr)
+            draw.text((612-((headerW+header_nameW)/2),-1), user_name, font=helvetica_neue_bold, fill=fill_clr)
             draw.text((612-((headerW+header_nameW)/2)+header_nameW,0), header, font=helvetica_neue, fill=fill_clr)
             draw.text((612-(titleW/2),40*2), final_title, font=titling_gothic, fill=title_clr)
+
+            draw.text(((612*2/2)-(subtitleW/2),(134+22)*2), subtitle, font=helvetica_neue, fill=fill_clr)
+            del draw
+            textImg = textImg.resize((612, 195), Image.ANTIALIAS)
+            black_to_transparent(textImg)
+            icon = getCategoryIcon(category, light)
+            icon = icon.convert('RGBA')
+            #if light:
+            #    invert_to_transparent(icon)
+            textImg.paste(icon, ((612-30)/2, 122), icon)
+
             if stamp:
                 stampImg = stamp.resize((38*2,38*2))
                 mask = Image.new('1', (38*2, 38*2), 0)
                 mask.paste(1, (0,0), stampImg)
                 textImg.paste(stampImg,((612-(titleW/2) + titleW-30), 36*2), mask)
 
-            draw.text(((612*2/2)-(subtitleW/2),(134+22)*2), subtitle, font=helvetica_neue, fill=fill_clr)
-            del draw
-            textImg = textImg.resize((612, 195), Image.ANTIALIAS)
-            icon = getCategoryIcon(category)
-            icon = icon.convert('RGBA')
-            if color is not None:
-                invert_to_transparent(icon)
-            textImg.paste(icon, ((612-28)/2-2, 122), icon)
-
             # add rule lines
-            if rule:
-                if rule == 'light':
-                    ruleImg = Image.open(self.__basepath + 'rule-light.png')
-                else:
-                    ruleImg = Image.open(self.__basepath + 'rule-dark.png')
-                textImg.paste(ruleImg, ((612-300)/2,134), ruleImg)
+                #if rule == 'light':
+                #    ruleImg = Image.open(self.__basepath + 'rule-light.png')
+                #else:
+            if light:
+                rule_left = Image.open(self.__basepath + 'rule-light.png')
+            else:
+                rule_left = Image.open(self.__basepath + 'rule-dark.png')
+            textImg.paste(rule_left, ((612-30)/2 - 40,131))
+            rule_rght = rule_left.transpose(Image.FLIP_LEFT_RIGHT)
+            textImg.paste(rule_rght, ((612-30)/2 + 40,131))
             return textImg
 
         def profile_img_popout(profile_img_url, img, y):
@@ -321,33 +339,34 @@ class Instagram(object):
             profileShadow = profileShadow.convert('RGBA')
             black_to_transparent(profileShadow)
             img.paste(profileShadow, ((612-112)/2, y), profileShadow)
-            img.paste(back, ((612-112)/2 + 5, y+5))
-            img.paste(profileImg, ((612-112)/2 + 8, y+8))
+            img.paste(back, ((612-112)/2 + 5, y+3))
+            img.paste(profileImg, ((612-112)/2 + 8, y+6))
 
 
 
         masks = [
             (270, 270, self.__basepath + 'stamp_mask.png'),
-            (612,  9,  self.__basepath + 'ribbon-top.png'),
-            (612,  9,  self.__basepath + 'ribbon-bottom.png'),
+            (612,  8,  self.__basepath + 'ribbon.png'),
+           # (612,  8,  self.__basepath + 'ribbon.png'),
         ]
 
         gradientImgs = [self.__imageDB.generate_gradient_image(primary_color, secondary_color, x[0], x[1], x[2])
                         for x in masks]
         stamp = gradientImgs[0]
         ribbon_top = gradientImgs[1]
-        ribbon_bot = gradientImgs[2]
-        shadow_top = Image.open(self.__basepath + 'shadow-top.png')
+        ribbon_bot = ribbon_top.transpose(Image.FLIP_TOP_BOTTOM)
+        #ribbon_bot = gradientImgs[2]
+        shadow_top = Image.open(self.__basepath + 'ribbon-shadow.png')
         shadow_bot = shadow_top.transpose(Image.FLIP_TOP_BOTTOM)
 
-        a =  1.77
-        b =  -1.61
-        c =  298
-        d =  1.074
-        e =  4.11
-        f =  -820
-        g =  -0.0001
-        h =  0.00215
+#        a =  1.77
+#        b =  -1.61
+#        c =  298
+#        d =  1.074
+#        e =  4.11
+#        f =  -820
+#        g =  -0.0001
+#        h =  0.00215
         x = -44
         y = 1
 
@@ -362,15 +381,15 @@ class Instagram(object):
             entityImg = utils.getWebImage(entity_img_url)
             if user_generated:
 
-                textImg = getInstagramTextImg(user_name, category, types, title, subtitle, stamp, '#ffffff', 'light')
-                textImgShadow = getInstagramTextImg(user_name, category, types, title, subtitle, None, '#000000', False)
-                for i in range(1):
-                    textImgShadow = textImgShadow.filter(ImageFilter.BLUR)
-                black_to_transparent(textImgShadow)
+                textImg = getInstagramTextImg(user_name, category, types, title, subtitle, stamp, False)
+                #textImgShadow = getInstagramTextImg(user_name, category, types, title, subtitle, None, '#000000', False)
+                #for i in range(1):
+                #    textImgShadow = textImgShadow.filter(ImageFilter.BLUR)
+                #black_to_transparent(textImgShadow)
                 entityImg = fill_img(entityImg)
                 img.paste(entityImg, (0, 0))
-                profile_img_popout(profile_img_url, img, 128)
-                img.paste(textImgShadow, (-1,249), textImgShadow)
+                profile_img_popout(profile_img_url, img, 126)
+                #img.paste(textImgShadow, (0,250), textImgShadow)
                 img.paste(textImg, (0, 248), textImg)
             else:
                 textImg = getInstagramTextImg(user_name, category, types, title, subtitle)
@@ -380,7 +399,7 @@ class Instagram(object):
                 img.paste(textImg, (0, 40), textImg)
         else:
             textImg = getInstagramTextImg(user_name, category, types, title, subtitle)
-            profile_img_popout(profile_img_url, img, 128)
+            profile_img_popout(profile_img_url, img, 126)
             img.paste(textImg, (0, 248), textImg)
 
             # Write the stamp url at the bottom
