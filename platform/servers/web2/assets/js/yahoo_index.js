0c684f549164abf3cdcc8821f091db5e426466a3
Travis Fischer <travis@stamped.com>
2012-10-24 15:33:02 -0400

    .

---
+++ platform/servers/web2/assets/js/yahoo_index.js
@@ -0,0 +1,61 @@
+/*! yahoo_index.js
+ * 
+ * Copyright (c) 2011-2012 Stamped Inc.
+ */
+
+/*jslint plusplus: true */
+/*global window, jQuery, $ */
+
+(function() {
+    $(document).ready(function() {
+        
+        // ---------------------------------------------------------------------
+        // initialize globals and utils
+        // ---------------------------------------------------------------------
+        
+        
+        var $body = $("body");
+        
+        $body.on("focus", "input", function(event) {
+            var $this = $(this);
+            var $form = $this.parent();
+            var value = $this.val().trim();
+            
+            if (value == "Your email or username") {
+                $this.attr("value", "");
+            }
+            
+            return true;
+        });
+        
+        $body.on("focusout", "input", function(event) {
+            var $this = $(this);
+            var $form = $this.parent();
+            var value = $this.val().trim();
+            
+            if (value.length <= 0) {
+                $this.attr("value", "Your email or username");
+            }
+            
+            return true;
+        });
+        
+        $body.on("submit", ".export-form", function(event) {
+            event.preventDefault();
+            
+            var $this   = $(this);
+            var $input  = $this.find("input");
+            var value   = $input.val().trim();
+            
+            if (!value) {
+                return false;
+            }
+            
+            console.log(value);
+            
+            window.location = "/export-stamps?login=" + value;
+            return false;
+        });
+    });
+})();
+
